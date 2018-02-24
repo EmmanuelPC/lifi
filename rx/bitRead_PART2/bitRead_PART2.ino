b58604771e6e2800bab05408e3dd0f201ea1f728
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-02-24 10:08:23 +0530

    lifi

---
+++ rx/bitRead_PART2/bitRead_PART2.ino
@@ -0,0 +1,30 @@
+#include <IRremote.h>
+IRsend irsend;
+int f=0,i=0,j=0,r=0;
+char value;
+void setup()
+{
+  Serial.begin(9600);
+} 
+void loop()
+{
+ irsend.sendSony(1, 2);
+ delay(40);
+ for(i=7; i>=0 ;i--)
+ {
+   if(analogRead(A0)>700)
+     r=1;
+    else
+     r=0;
+  delay(35);
+  bitWrite(value,i,r);
+ }
+ 
+  if(value==47)
+    Serial.println("");
+  else
+    Serial.print(value);
+ irsend.sendSony(2, 2);// 2 send data 2 bit size
+ delay(40);
+}
+
---
+++ tx/bitRead_PART1/bitRead_PART1.ino
@@ -0,0 +1,50 @@
+#include <IRremote.h>
+IRrecv irrecv(11);
+decode_results results;
+int i=0;
+char n[]="/GOD IS LOVE/";
+int s=sizeof(n);
+int d1=10;
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(s);
+  pinMode(13,OUTPUT);
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn();
+}
+void loop()
+{
+
+  delay(d1);
+  if (irrecv.decode(&results)) {
+    irrecv.resume(); 
+  }
+  if(results.value==1)
+ { 
+  int a[10],l=0, j=0;
+   if(s==(i+1))
+       i=0;
+  if(n[i]!='\0')
+  {
+    int c=n[i];
+    if(a[i]==42)
+     Serial.println("");
+    else
+     Serial.print(n[i]);
+    for(j=7 ; j>=0 ;j--)
+     {
+      int l=bitRead(c,j);
+      digitalWrite(13,l);     
+      delay(35);
+     }
+    i++;
+    }
+ }
+   results.value=0;
+    delay(d1);
+   if (irrecv.decode(&results)) {
+    irrecv.resume(); 
+  }
+  }
+}
